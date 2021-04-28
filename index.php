6165b145e1052c16f44013646c3f1dd053d6209d
Gonchikov Ayur <hori.tumat@gmail.com>
2021-04-28 22:00:11 +0800

    homework lesson1

--- index.php
+++ index.php
@@ -1,57 +1,102 @@
 <?php
+
 declare(strict_types=1);
 
 ini_set('error_reporting', (string)E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
-abstract class MyAbstactProduct {
-  protected $quantity;
-  protected $price;
-  function __construct( int $quantity, int $price)
-  {
-    $this->quantity = $quantity;
-    $this->price = $price;
-  }
-  abstract protected function FinalСost();
-  public function getCost()
-  {
-    echo $this->FinalСost();
-  }
-}
+class ShippedClass
+{
+  public $trackNumber;
+  public $company;
 
-class DigitalProduct extends MyAbstactProduct {
-  protected function FinalСost()
+  function __construct(string $trackNumber, $company)
   {
-    return "digital product cost = " . $this->quantity * $this->price . "<br>";
+    $this->trackNumber = $trackNumber;
+    $this->company = $company;
   }
-}
 
-class PieceGoods extends MyAbstactProduct {
-  protected function FinalСost()
+  public function getTrackNumber()
   {
-    return "piece goods cost = " . $this->quantity * $this->price . "<br>";
+    return $this->trackNumber;
+  }
+
+  public function getCompany()
+  {
+    return $this->company;
+  }
+
+  public function viewTrackNumberAndCompany()
+  {
+    echo "<h1>$this->getTrackNumber</h1><br/><p>$this->company</p>";
   }
 }
 
-class WeightGoods extends MyAbstactProduct {
-  protected function FinalСost()
+class statusDelivery extends ShippedClass
+{
+  public $status;
+  function __construct($status, $trackNumber, $company)
   {
-    if ($this->quantity < 2.5)
-      return "weight goods cost = " . $this->quantity * $this->price . "<br>";
-    if ($this->quantity > 2.5 && $this->quantity < 20)
-      return "weight goods cost = " . $this->quantity * ($this->price - $this->price * 0.1) . "<br>";
-    else {
-      return "weight goods cost = " . $this->quantity * $this->price * 0.25 . "<br>";
-    }
+    parent::__construct($trackNumber, $company);
+    $this->status = $status;
+  }
+  function viewInfo()
+  {
+    parent::getTrackNumber();
+    echo "TrackNumber: $this->status";
   }
 }
 
-$DP = new DigitalProduct(2, 45);
-$DP->getCost();
+// вывод "1234" потому что мы не используем псевдопеременную указываующий в классе переменную для каждого экземпляра
+class A
+{
+  public function foo()
+  {
+    static $x = 0;
+    echo ++$x;
+  }
+}
+$a1 = new A(); //Создаем экземпляр где метод
+$a2 = new A(); //Создаем экземпляр тот же где функция эта ссылка на функцию в классе
+$a1->foo(); // вызов функции увеличиваем на 1
+$a2->foo(); // вызов функции увеличиваем на 1
+$a1->foo(); // вызов функции увеличиваем на 1
+$a2->foo(); // вызов функции увеличиваем на 1
 
-$PG = new PieceGoods(3, 45);
-$PG->getCost();
+class A
+{
+  public function foo()
+  {
+    static $x = 0;
+    echo ++$x;
+  }
+}
+class B extends A
+{
+}
+$a1 = new A(); // Здесь создаем экземляр $а1 из класса А
+$b1 = new B(); // Здесь создается новый экземпляр из класса В
+$a1->foo(); // вызов функции увеличиваем на 1 экземпляр а1 = 1
+$b1->foo(); // вызов функции увеличиваем на 1 экземпляр b1 = 2
+$a1->foo(); // вызов функции увеличиваем на 1 экземпляр а1 = 1
+$b1->foo(); // вызов функции увеличиваем на 1 экземпляр b1 = 2
 
-$WG = new WeightGoods(21, 45);
-$WG->getCost();
+class A
+{
+  public function foo()
+  {
+    static $x = 0;
+    echo ++$x;
+  }
+}
+class B extends A
+{
+}
+// В отсутсвии аргументов в конструктуре класса, круглые скобки можно опустить
+$a1 = new A;
+$b1 = new B;
+$a1->foo();
+$b1->foo();
+$a1->foo();
+$b1->foo();
